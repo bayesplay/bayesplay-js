205a2a6028f67847fc6a0eb6586d67d7d4ca07b2
Lincoln Colling <lincoln@colling.net.nz>
2026-03-04 19:39:14 +0000

    forgot the export

--- index.ts
+++ index.ts
@@ -1,3 +1,3 @@
 import {bayesfactor, type NormalLikelihood, type NormalPrior, type PointPrior, type UniformPrior, likelihood } from "./bayesfactor";
 
-export { bayesfactor, type NormalLikelihood, type NormalPrior, type PointPrior, type UniformPrior };
+export { bayesfactor, type NormalLikelihood, type NormalPrior, type PointPrior, type UniformPrior, likelihood };
